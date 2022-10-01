2ebd25f4c670a98e2d0c88268e55ed8ae95adf95
1convitxoera2caicanh <thduong.cao@gmail.com>
2022-10-01 12:47:15 +0700

    doi mau cho nut

--- main1.js
+++ main1.js
@@ -1,8 +1,17 @@
 const numberAndOperator = document.querySelectorAll("span[data-key]");
 console.log(numberAndOperator)
 let userElement = ''
+let color = ["red","blue","black","orange","green","grey","yellow","pink"]
+let counter = 1
 Array.prototype.forEach.call(numberAndOperator,function(item) {
     item.addEventListener("click",function(){
+        if(counter<=7){
+            item.style.backgroundColor = color[counter]
+            counter +=1
+        }
+        else {
+            counter = 0
+        }
         let item_value = item.getAttribute("data-key")
         console.log(item_value)
         userElement += item_value;
