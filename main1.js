0880213567f9f499ba1183e6834bac0e6a089276
1convitxoera2caicanh <thduong.cao@gmail.com>
2022-10-01 12:30:29 +0700

    calculator

---
+++ main1.js
@@ -0,0 +1,39 @@
+const numberAndOperator = document.querySelectorAll("span[data-key]");
+console.log(numberAndOperator)
+let userElement = ''
+Array.prototype.forEach.call(numberAndOperator,function(item) {
+    item.addEventListener("click",function(){
+        let item_value = item.getAttribute("data-key")
+        console.log(item_value)
+        userElement += item_value;
+        console.log(userElement)
+        input_screen.textContent = userElement
+        console.log(typeof item_value)
+        returnResult()
+    })
+})
+
+const equal = document.querySelector(".equals-to")
+equal.addEventListener("click",function(){
+    returnResult()
+
+})
+
+const input_screen = document.querySelector(".input")
+const result_screen = document.querySelector(".result")
+
+const delete_button = document.querySelector(".delete")
+delete_button.addEventListener("click",function(){
+    result_screen.textContent = ""
+   userElement = userElement.slice(0,-1)
+    input_screen.textContent = userElement
+    returnResult() 
+
+})
+
+function returnResult() {
+    if (!Number.isNaN(parseInt(userElement[userElement.length-1]))) {
+        const result = eval(userElement)
+        result_screen.textContent = result
+        }
+}
